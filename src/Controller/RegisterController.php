646f4ada48e90f50f5360bb9f1e168ced4f6574b
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-04-08 12:44:17 +0200

    feat: add first part of logic for register form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +15,29 @@ class RegisterController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $client = new Client();
+
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('save', SubmitType::class, ['label' => 'Créer mon compte'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $client = $form->getData();
+
+            dd($client);
+        }
+
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView(),
         ]);
     }
 }
