97dc8de368679b2e4c137850a3ca8214a87ce60a
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-04-08 13:54:24 +0200

    feat: add dependancy for upload file

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
