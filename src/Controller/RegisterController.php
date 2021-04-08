5ef8c6c5fbf9abee9e5cd8ba23843feca969b799
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-04-08 13:08:00 +0200

    feat: add second part of user form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,8 +27,13 @@ class RegisterController extends AbstractController
         $client = new Client();
 
         $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('firstname', TextType::class)
+            ->add('birthdate', BirthdayType::class, ['years' => range(date('Y') - 70, date('Y'))])
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
+            ->add('address', TextType::class)
+            ->add('zipcode', NumberType::class, ['html5' => true])
             ->add('save', SubmitType::class, ['label' => 'Créer mon compte'])
             ->getForm();
 
