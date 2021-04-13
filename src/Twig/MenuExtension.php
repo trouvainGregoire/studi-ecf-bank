fafff4d8af4b30e48c6296496a3c329ce186838d
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-04-13 14:22:55 +0200

    style : remove useless js script for banker

--- src/Twig/MenuExtension.php
+++ src/Twig/MenuExtension.php
@@ -20,7 +20,8 @@ class MenuExtension extends AbstractExtension
     {
         $menuRequiredPaths = ['client_dashboard', 'client_show_recipients',
             'client_create_transaction', 'client_create_recipient',
-            'client_delete_account'];
+            'client_delete_account', 'banker_dashboard', 'banker_pending_accounts', 'banker_pending_recipients',
+            'banker_pending_removal_accounts'];
 
         return in_array($path, $menuRequiredPaths);
     }
